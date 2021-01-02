4e8235363dc383379a1e05c2891fd730b6ccf138
LeeJaeWon-Rbt <jawwoni@naver.com>
2021-01-02 15:10:21 +0900

    headerfile

---
+++ 클래스와객체모듈화/Calculator.h
@@ -0,0 +1,10 @@
+#ifndef CALCULATOR_H_
+#define CALCULATOR_H_
+
+class Calculator {
+public:
+	void run();
+};
+
+#endif // !CALCULATOR_H_
+
---
+++ 클래스와객체모듈화/processer.h
@@ -0,0 +1,13 @@
+#ifndef PROCESSER_H_
+#define PROCESSER_H_
+
+class processer {
+private:
+	int op1, op2;
+public:
+	processer(int, int);
+	void process(int*, int*, int*, double*); //ÇÕ Â÷ °ö Á¦
+};
+
+#endif // !PROCESSER_H_
+
